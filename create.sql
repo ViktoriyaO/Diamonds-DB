94629f40b981a940e104fd7cb34fde011d439115
ViktoriyaO <43511927+ViktoriyaO@users.noreply.github.com>
2020-04-17 11:37:22 +0300

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Diamond(
+    diamond_index INT NOT NULL,
+    color CHAR(1) NOT NULL,
+    cut VARCHAR2(9) NOT NULL,
+    clarity VARCHAR2(4) NOT NULL,
+    price INT NOT NULL,
+    carat_weight NUMBER(3,2) NOT NULL);
+ALTER TABLE Diamond
+    ADD CONSTRAINT diamond_index_pk PRIMARY KEY(diamond_index);
+
+CREATE TABLE Description(
+    color CHAR(1) NOT NULL);
+ALTER TABLE Description
+    ADD CONSTRAINT color_pk PRIMARY KEY(color);    
+    
+CREATE TABLE Category(
+    cut VARCHAR2(9) NOT NULL);
+ALTER TABLE Category
+    ADD CONSTRAINT cut_pk PRIMARY KEY(cut);    
+    
+CREATE TABLE Clarity(
+    clarity VARCHAR2(4) NOT NULL);
+ALTER TABLE Clarity
+    ADD CONSTRAINT clarity_pk PRIMARY KEY(clarity);
+    
+ALTER TABLE Diamond
+    ADD CONSTRAINT color_fk FOREIGN KEY(color) REFERENCES Description(color);
+ALTER TABLE Diamond
+    ADD CONSTRAINT cut_fk FOREIGN KEY(cut) REFERENCES Category(cut);
+ALTER TABLE Diamond
+    ADD CONSTRAINT clarity_fk FOREIGN KEY(clarity) REFERENCES Clarity(clarity);
