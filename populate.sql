259f531c8d6754a477f2c531c6fcc5b642287149
ViktoriyaO <43511927+ViktoriyaO@users.noreply.github.com>
2020-05-05 18:14:26 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,29 +1,3 @@
---Додавання інформації в таблицю Diamond
-
-INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
-VALUES ('1', 'E', 'Ideal', 'SI2', '326', '0.23');
-
-INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
-VALUES ('2', 'E', 'Premium', 'SI1', '326', '0.21');
-
-INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
-VALUES ('3', 'F', 'Good', 'VS1', '327', '0.23');
-
-INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
-VALUES ('4', 'I', 'Premium', 'VS2', '334', '0.29');
-
-INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
-VALUES ('5', 'G', 'Good', 'VVS2', '335', '0.31');
-
-INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
-VALUES ('6', 'J', 'Very Good', 'I1', '336', '0.24');
-
-INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
-VALUES ('7', 'D', 'Very Good', 'SI2', '336', '0.24');
-
-INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
-VALUES ('8', 'H', 'Fair', 'VVS1', '337', '0.26');
-
 --Додавання інформації в таблицю  Desctiption
 
 INSERT INTO Description (color) VALUES ('D');
@@ -69,3 +43,29 @@ INSERT INTO Clarity (clarity) VALUES ('SI1');
 INSERT INTO Clarity (clarity) VALUES ('SI2');
 
 INSERT INTO Clarity (clarity) VALUES ('I1');
+
+--Додавання інформації в таблицю Diamond
+
+INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
+VALUES ('1', 'E', 'Ideal', 'SI2', '326', '0.23');
+
+INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
+VALUES ('2', 'E', 'Premium', 'SI1', '326', '0.21');
+
+INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
+VALUES ('3', 'F', 'Good', 'VS1', '327', '0.23');
+
+INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
+VALUES ('4', 'I', 'Premium', 'VS2', '334', '0.29');
+
+INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
+VALUES ('5', 'G', 'Good', 'VVS2', '335', '0.31');
+
+INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
+VALUES ('6', 'J', 'Very Good', 'I1', '336', '0.24');
+
+INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
+VALUES ('7', 'D', 'Very Good', 'SI2', '336', '0.24');
+
+INSERT INTO Diamond (diamond_index, color, cut, clarity, price,carat_weight)
+VALUES ('8', 'H', 'Fair', 'VVS1', '337', '0.26');
